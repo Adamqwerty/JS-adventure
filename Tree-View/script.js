359a9c8bfe596e2380a7f5015ddf137fc1f9a929
Adamqwerty <https://github.com/Adamqwerty/Flexbox.git>
2019-04-03 21:53:28 +0200

    js file and Tree-View done

---
+++ Tree-View/script.js
@@ -0,0 +1,8 @@
+let toggler = document.querySelectorAll(".caret");
+
+for (let i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+} 
